5cf881630658328e5f5a68a1ce4848a0f74a5e58
Paul Pacaud <paul.pacaud@student-cs.fr>
2023-10-24 14:10:22 +0200

    added index.ts

---
+++ index.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
+
+async function handler(_req: Request): Promise<Response> {
+    return new Response("Hello World");
+}
+
+serve(handler);
